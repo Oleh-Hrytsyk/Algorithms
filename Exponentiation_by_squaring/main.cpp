87f863ffa804880090f5440b26bfb7d5a5f617a4
Oleh-Hrytsyk <oleh.hrytsyk.it.2017@lpnu.ua>
2021-06-19 22:21:47 +0300

    Created "exponentiationBySquaring" function.

--- Exponentiation_by_squaring/main.cpp
+++ Exponentiation_by_squaring/main.cpp
@@ -1,9 +1,22 @@
 #include <iostream>
 
-using namespace std;
+long long exponentiationBySquaring(long long number, const int exponent){
+    long long result = 1;
+    if (number == 0)
+        return 0;
+    else{
+        for(int i = 0; (exponent >> i) != 0; i++){
+            if(((exponent >> i) & 1) == 1){
+                result*=number;
+            }
+            number*=number;
+        }
+    }
+    return result;
+}
 
 int main()
 {
-    cout << "Hello World!" << endl;
+    std::cout << exponentiationBySquaring(9, 9) << std::endl;
     return 0;
 }
